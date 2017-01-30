fe8562a74554bfeef8449aae40f91f4a5931878e
Hanked <dave.kingsnorth@photobox.com>
2017-01-30 00:15:46 +0000

    minimal viable store with async http request

--- app/src/main.js
+++ app/src/main.js
@@ -1,13 +1,15 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
 
 import App from './App';
 import router from './routes';
 import { store } from './store/store';
 
-window.Vue = Vue;
-
 Vue.use(VueRouter);
+Vue.use(VueResource);
+
+window.Vue = Vue;
 
 new Vue({
   el: '#app',
--- app/src/store/actions.js
+++ app/src/store/actions.js
@@ -1,8 +1,20 @@
+import Vue from 'vue';
 import * as types from './types';
 
 export default {
     [types.UPDATE_GREETING]: ({commit}, payload) => {
-        console.log('UPDATE_GREETING', payload)
-        commit(types.MUTATE_GREETING, payload)
+        console.log('UPDATE_GREETING', payload);
+
+        Vue.http.get('http://localhost:3000/api/users')
+          .then(function(res) {
+            fetchMemebersSuccess(res)
+          })
+          .catch(function(res) {
+            console.log('failed to fetch members');
+          })
+
+        function fetchMemebersSuccess(res) {
+          commit(types.MUTATE_GREETING, res.body[payload].fullName)
+        }
     }
 };
