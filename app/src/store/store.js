44a5cd2a85da201f3273090902a38e880e9ef594
Hanked <dave.kingsnorth@photobox.com>
2017-03-02 00:49:43 +0000

    separate user and follows in to different modules

--- app/src/store/store.js
+++ app/src/store/store.js
@@ -2,15 +2,17 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import VueResource from 'vue-resource';
 
-import members from './modules/members';
 import user from './modules/user';
+import members from './modules/members';
+import follows from './modules/follows';
 
 Vue.use(Vuex);
 Vue.use(VueResource);
 
 export const store = new Vuex.Store({
   modules: {
+    user,
     members,
-    user
+    follows
   }
 });
