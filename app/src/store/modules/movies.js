baa40ce58ebd809bea8d37826a1afb0e621305ea
Hanked <dave.kingsnorth@photobox.com>
2017-03-10 17:45:35 +0000

    create action to find and save movie

--- app/src/store/modules/movies.js
+++ app/src/store/modules/movies.js
@@ -1,7 +1,8 @@
 import Vue from 'vue';
 
 const state = {
-
+  userMovies: [],
+  memberMovies: []
 };
 
 const getters = {
@@ -9,19 +10,91 @@ const getters = {
 };
 
 const mutations = {
-
+  SET_MOVIES: (state, movies) => {
+    state.userMovies = movies;
+  },
+  UPDATE_MOVIES: (state, movie) => {
+    state.userMovies.push(movie);
+  }
 };
 
 const actions = {
-  ADD_MOVIE: ({ commit, dispatch }, searchTerm) => {
-    Vue.http.get(`http://www.omdbapi.com/?t=${searchTerm}&y=&plot=short&r=json`)
+  INIT_MOVIES: ({ commit, dispatch, rootState}) => {
+    // console.log(rootState.user);
+    const token = localStorage.getItem('token');
+    const userId = localStorage.getItem('userId');
+    if (!token) { return }
+
+    Vue.http.get(`http://localhost:3000/api/movies/${userId}`,
+      {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      })
       .then(function(res) {
-        console.log(res.body);
+        commit('SET_MOVIES', res.body);
       })
       .catch(function(res) {
-        console.log('could not get movie');
+        console.log('failed to init movies');
       })
+  },
+
+  ADD_MOVIE: ({ commit, dispatch }, searchTerm) => {
+    const userId = localStorage.getItem('userId');
+    if (!userId) { return }
+
+    Vue.http.get(`http://www.omdbapi.com/?t=${searchTerm}&y=&plot=short&r=json`)
+      .then(function(res) {
+
+        // if movie not found
+        if (res.body.Response == 'False') { return };
+
+        commit('UPDATE_MOVIES', {
+          userId: userId,
+          movieId: res.body.imdbID,
+          watched: false,
+          recommended: false,
+          disliked: false,
+          position: 0
+        });
+
+        dispatch('SAVE_MOVIE', res.body.imdbID);
+      })
+      .catch(function(res) {
+        console.log('could not get movie', res);
+      })
+  },
+
+  SAVE_MOVIE: ({ commit }, movieId) => {
+    const userId = localStorage.getItem('userId');
+    const token = localStorage.getItem('token');
+    if (!token) { return }
+
+    Vue.http.post(`http://localhost:3000/api/movies/${userId}`,
+      {
+        movieId: movieId
+      },
+      {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      })
+      .then(function(res) {
+        console.log('movie saved');
+      })
+      .catch(function(res) {
+        console.log('failed to save movie');
+      })
+  },
+
+  REMOVE_MOVIE: ({ commit, dispatch }) => {
+
+  },
+
+  UPDATE_MOVIE: ({ commit, dispatch }) => {
+
   }
+
 };
 
 export default {
