c4ba6b9b7fcfaac1c31ced488f5af1609b110638
Hanked <dave.kingsnorth@photobox.com>
2017-03-10 15:36:06 +0000

    create addMovie comp to replace static html in profile view

---
+++ app/src/components/profile/addMovie.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="add-movie">
+    <div class="notification search is-primary">
+      <p class="control has-addons">
+        <input class="input is-expanded" type="text" v-model="movieSearchTerm" @keydown.enter="addMovie">
+        <a class="button is-primary is-active" @click="addMovie">
+          Add
+        </a>
+      </p>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'add-movie',
+
+  data() {
+    return {
+      movieSearchTerm: ''
+    }
+  },
+
+  methods: {
+    addMovie() {
+      this.movieSearch();
+      this.movieSearchTerm = '';
+    },
+
+    movieSearch() {
+      this.$store.dispatch('ADD_MOVIE', this.movieSearchTerm);
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+  .search {
+    margin-bottom: 15px;
+  }
+</style>
